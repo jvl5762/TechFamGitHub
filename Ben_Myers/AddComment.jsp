13b45ae9083591d247ba6b481911ccd735913674
Benjamin Myers <myersben2@gmail.com>
2016-10-28 16:17:58 -0400

    Add files via upload

--- Ben_Myers/AddComment.jsp
+++ Ben_Myers/AddComment.jsp
@@ -7,22 +7,36 @@
 <html>
 <body>
 <%
+	// NOTE THAT ABOVE WHERE THE IMPORT = TEST.ADDREVIEW MIGHT CHANGE DEPENDING ON WHICH PACKAGE YOU PUT
+	// THE JAVA FILE IN
+
+	// HOW TO CREATE THE ADDREVIEW CLASS
 	AddReview ar = new AddReview();
 	String return_val = ar.addNewReview("abby1", "4", "5", "moo");
-	if(return_val.equals("success\n")){
+	
+	//IF return_val == success then the item was added correctly
+	// otherwise return_val holds a string of what went wrong
+	// you may want to print this out to the user to check for errors
+	if(return_val.equals("success")){
 		out.println("yippee\n");
 	}else{
 		out.println(return_val);
 	}
 	
+	// Use this to get all reviews for supplier 4
 	ResultSet rs = ar.getReviews("4");
+	// check if rs is null first
 	if(rs != null){
+		// cycle through all reviews and get the elements you want
+		// note that these print statements are currently formatted pretty badly
+		// you may want to change how it prints or add the strings to some sort of template
 		while(rs.next()){
 			out.println(rs.getString("username") + " | Rating: " + rs.getFloat("value") );
 			out.println(rs.getString("explanation") + "\n");
 		}
 		
 		try{
+			// always close rs when finished if not null
 			rs.close();
 		}catch(Exception e){}
 	}
--- Ben_Myers/AddReview.java
+++ Ben_Myers/AddReview.java
@@ -3,6 +3,9 @@ import java.sql.*;
 import javax.sql.*;
 
 public class AddReview {
+	
+	// YOU MAY NEED TO MODIFY THESE, THIS IS MY TABLE, USERNAME, AND PASSWORD FOR MY TABLE
+	// YOUR TABLE MAY BE DIFFERENT
 	public static final	String DATABASE_NAME = "techfamforever";
 	public static final String DATABASE_USERNAME = "root";
 	public static final String DATABASE_PASSWORD = "TechFam";
@@ -10,75 +13,95 @@ public class AddReview {
 			"?autoReconnect=true&useSSL=false";
 	public static final String DRIVER_LOC = "com.mysql.jdbc.Driver";
 	
+	// MAX SIZE THE DESCRIPTION CAN BE
 	public static final int MAX_DESCRIPTION_SIZE = 128;
 	
+	// SQL STATEMENTS ADDING RATING
 	public static final String CHECK_IF_EXIST_SQL = "SELECT * FROM Rating R WHERE R.username = ? And R.supplier_id = ?";
 	public static final String GET_HIGHEST_RATING_SQL = "SELECT MAX(rating_id) FROM Rating R WHERE R.supplier_id = ?";
 	public static final String ADD_RATING_SQL = "INSERT INTO Rating VALUES (?,?,?,?,?)";
 	
+	// SQL STATEMENT FOR GETTING RATINGS
 	public static final String GET_RATINGS_FOR_SUPPLIER = "SELECT * FROM Rating R WHERE R.supplier_id = ?";
 	
+	// CONSTANT STRING TO RETURN WHEN SUCCELSSFULLY ADDED NEW ENTRY
 	public static final String SUCCESS_STRING = "success";
 	
 	public AddReview(){}
 	
 	public String addNewReview(String username, String seller_id_str, String rating_str, String description) {
-		
+		// STRING TO BE RETURNED
 		String return_statement = "";
 		
+		// CONVERT STRING PARAMETERS TO PROPER FORM
 		Integer seller_id = Integer.parseInt(seller_id_str);
 		Float rating = Float.parseFloat(rating_str);
 		Integer ratingID = 1;
 		
+		
+		// USED FOR CONNECTING TO DATABASE
 		Connection con = null;
 		PreparedStatement check_if_exists = null;
 		PreparedStatement getHighestRatingID = null;
 		PreparedStatement addRating = null;
 		
+		
+		// MAKE SURE NO PARAMETER IS NULL
 		if(username == null || seller_id == null || rating == null || description == null){
 			return "Missing parameter";
 		}
 		
+		// MAKE SURE DESCRIPTION IS FILLED OUT
 		if(description.isEmpty()){
 			return "Empty Description, Please enter a comment";
 		}
 		
+		// MAKE SURE DESCRIPTION IS NOT TOO LONG
 		if(description.length()>MAX_DESCRIPTION_SIZE){
 			return "Description size is too large";
 		}
 		
 		try{
+			// CONNECT TO DATABASE
 			Class.forName(DRIVER_LOC);
 			con = DriverManager.getConnection(DATABASE_CONNECT_STRING,DATABASE_USERNAME, DATABASE_PASSWORD);
 			
+			// CREATE SQL STATEMENT TO CHECK THAT THE USER HAS NOT ALREADY MADE A REVIEW FOR THE SELLER
 			check_if_exists = con.prepareStatement(CHECK_IF_EXIST_SQL);
 			check_if_exists.setString(1, username);
 			check_if_exists.setInt(2, seller_id);
 			ResultSet existingEntries = check_if_exists.executeQuery();
+			
+			// IF USER ALREADY SUBMITTED THEN RETURN STATEMENT THAT SAYS SO
 			if(existingEntries.next()){
 				return_statement = "User Already submitted";
 			}else{
 				try{
+					
+					// GET THE HIGHEST RATING ID AND MAKE THE NEXT RATING_ID +1 THAT RATING
+					// NOTE WE MAY WANT TO GET RID OF RATING ID IN THE FUTURE
 					getHighestRatingID = con.prepareStatement(GET_HIGHEST_RATING_SQL);
 					getHighestRatingID.setInt(1, seller_id);
 					existingEntries = getHighestRatingID.executeQuery();
+					
+					// IF RATING_ID ALREADY EXISTS, ADD 1 TO IT, OTHERWISE DEFAULT TO 1
 					if(existingEntries.next()){
 						ratingID = existingEntries.getInt("MAX(rating_id)") + 1;
 					}
 				
 					try{
-						System.out.println("start");
+						// ADD RATING
 						addRating = con.prepareStatement(ADD_RATING_SQL);
 						addRating.setInt(1, ratingID);
 						addRating.setString(2, description);
 						addRating.setFloat(3, rating);
 						addRating.setString(4, username);
 						addRating.setInt(5, seller_id);
-						System.out.println(username);
 						addRating.executeUpdate();
+						
+						// RETURN THIS IF RATING ADD IS SUCCESS
 						return_statement = SUCCESS_STRING;
 					}catch(Exception e){
-						System.out.println(e.getMessage());
 						return_statement = "Failed to add";
 					}
 				}catch(Exception e){
@@ -86,7 +109,6 @@ public class AddReview {
 				}
 			}
 		}catch(Exception e){
-			System.out.println(e.getMessage());
 			return_statement = "Check if existing entry failed";
 		}finally{
 			try{
@@ -94,17 +116,24 @@ public class AddReview {
 			}catch(Exception e){}
 		}
 		
+		// RETURN THE STATEMENT OF SUCCESS OR THE ERROR MESSAGE
 		return return_statement;
 	}
 
 	public ResultSet getReviews(String seller_id_str){
+		// CONVERT SELLER ID TO AN INTEGER
 		Integer seller_id = Integer.parseInt(seller_id_str);
+		
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
+		
 		try{
+			// CONNECT TO DATABASE
 			Class.forName(DRIVER_LOC);
 			con = DriverManager.getConnection(DATABASE_CONNECT_STRING,DATABASE_USERNAME, DATABASE_PASSWORD);
+			
+			// GET ALL RATING INFO FOR GIVEN SUPPLIER
 			ps = con.prepareStatement(GET_RATINGS_FOR_SUPPLIER);
 			ps.setInt(1, seller_id);
 			rs = ps.executeQuery();
@@ -113,7 +142,8 @@ public class AddReview {
 				con.close();
 			}catch(Exception e2){}
 		}
-	
+		
+		// RETURN SUPPLIER INFO
 		return rs;
 	}
 	
